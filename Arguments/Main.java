c4c2c1efbbc1fe0eaac19550e06bba365162cb7d
Laksara <40088875+Laksara1996@users.noreply.github.com>
2025-10-23 18:25:59 +0530

    [] - Scanner

--- Arguments/Main.java
+++ Arguments/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main (String[] args){
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello " + name);
+        System.out.println("Your are " + age + " years old");
+
+        scanner.close();
     }
 }
