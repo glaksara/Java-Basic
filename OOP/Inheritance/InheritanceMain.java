29b6d00e57ae65f8d0eaa66d8fb3e4008e9c6eb9
Laksara <40088875+Laksara1996@users.noreply.github.com>
2025-10-24 17:22:17 +0530

    [] - Super keyword

--- OOP/Inheritance/InheritanceMain.java
+++ OOP/Inheritance/InheritanceMain.java
@@ -1,5 +1,8 @@
 package Inheritance;
 
+import Inheritance.Super.Person;
+import Inheritance.Super.Student;
+
 public class InheritanceMain {
     public static void runClass() {
         Dog dog = new Dog();
@@ -17,5 +20,13 @@ public class InheritanceMain {
         Plant plant = new Plant();
         System.out.println(plant.isAlive);
         plant.photosynthesis();
+
+//        Super Key Word
+        Person person = new Person("Harry", "Potter");
+        person.showName();
+
+        Student student = new Student("Harry", "Potter", 3.35);
+        student.showName();
+        student.show();
     }
 }
