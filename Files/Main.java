419d964accd0bda84d925cd79519f8b74863dd8c
Laksara <40088875+Laksara1996@users.noreply.github.com>
2025-10-25 16:35:48 +0530

    [] - Read File

--- Files/Main.java
+++ Files/Main.java
@@ -1,16 +1,32 @@
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+//        Write
+        String content = """
+                Multiple lines test
+                Print two lines
+                """;
         try (FileWriter writer = new FileWriter("test.txt")) {
-            writer.write("I like pizza");
+            writer.write(content);
             System.out.println("File has been written");
         } catch (FileNotFoundException e) {
             System.out.println("Could not locate file location");
         } catch (IOException e) {
             System.out.println("Could not write file");
+        }
+
+//        Read
+        String filePath = "test.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
         }
 
     }
